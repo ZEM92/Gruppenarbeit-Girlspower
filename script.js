d38ed74c4bbb4e32fbae8487e541cdbce8370178
MarieWillersinn <mariewillersinn@gmail.com>
2022-06-29 09:01:39 +0200

    draganddrop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setURL('http://developerakademie.com/smallest_backend_ever-master');
-let users = []; 
+let users = [];
 
 async function initBackend() {
     await downloadFromServer();
@@ -22,4 +22,52 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+}
+
+
+
+let todos = [{
+
+    'id': 0,
+    'title': 'putzen',
+    'category': 'open'
+}, {
+    'id': 1,
+    'title': 'kochen',
+    'category': 'open'
+}, {
+    'id': 2,
+    'title': 'einkaufen',
+    'category': 'closed'
+}];
+
+let currentDraggedElement;
+
+function updateHTML() {
+    let open = todos.filter(t => t['category'] == 'open');
+
+    document.getElementById('open').innerHTML = '';
+
+    for (let index = 0; index < open.length; index++) {
+        const element = open[index];
+        document.getElementById('open').innerHTML += generateTodoHTML(element);
+    }
+
+    let closed = todos.filter(t => t['category'] == 'closed');
+    document.getElementById('closed').innerHTML = '';
+
+    for (let index = 0; index < closed.length; index++) {
+        const element = closed[index];
+        document.getElementById('closed').innerHTML += generateTodoHTML(element);
+    }
+}
+
+function startDragging(id) {
+
+    let currentDraggedElement = id;
+
+}
+
+function generateTodoHTML(element) {
+    return `<div draggable="true" ondraggstart="startDragging(${element['id']})" class="todo">${element['title']}</div>`;
 }
